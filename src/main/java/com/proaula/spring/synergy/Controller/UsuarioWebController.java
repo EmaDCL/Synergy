7ced2a077b6dee437a59f3352b702f9ece7ece58
Jesús Castilla <jesusdanielcastillaorozco@gmail.com>
2025-11-17 15:16:57 -0500

    Arreglos extras

--- src/main/java/com/proaula/spring/synergy/Controller/UsuarioWebController.java
+++ src/main/java/com/proaula/spring/synergy/Controller/UsuarioWebController.java
@@ -1,6 +1,7 @@
-package com.proaula.spring.synergy.Controller;
+package com.proaula.spring.synergy.Controller; // ← Minúscula
 
 import com.proaula.spring.synergy.Model.Usuarios;
+import com.proaula.spring.synergy.Model.Usuarios.Rol;
 import com.proaula.spring.synergy.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,26 +10,52 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UsuarioWebController {
-
+    
     @Autowired
     private UsuarioService usuarioService;
+    
 
-
+    @GetMapping("/")
+    public String mostrarIndex() {
+        return "index";
+    }
+    
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+    
+   
     @GetMapping("/registrarse")
     public String mostrarRegistro(Model model) {
         model.addAttribute("usuario", new Usuarios());
         return "registro";
     }
-
+    
     @PostMapping("/registrarse")
-    public String registrar(@ModelAttribute("usuario") Usuarios usuario) {
+    public String registrar(@ModelAttribute("usuario") Usuarios usuario, 
+                           @RequestParam(required = false) String rol) {
+  
+        if (rol != null && !rol.isEmpty()) {
+            usuario.setRol(Rol.valueOf(rol));
+        } else {
+            usuario.setRol(Rol.Participante); 
+        }
+        
         usuarioService.guardarUsuario(usuario);
         return "redirect:/login";
     }
+    
 
     @GetMapping("/login")
-    public String mostrarLogin(Model model){
+    public String mostrarLogin(Model model) {
         model.addAttribute("loginDTO", new Object());
         return "login";
+    }
+    
+
+    @GetMapping("/dashboard")
+    public String mostrarDashboard(Model model) {
+        return "dashboard_Usuario";
     }
 }
