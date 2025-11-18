def76cc1df5dcf52c6be6cea26cb6d91de900043
Jesús Castilla <jesusdanielcastillaorozco@gmail.com>
2025-11-18 10:55:52 -0500

    Reparacion index

--- src/main/java/com/proaula/spring/synergy/Model/Comentario.java
+++ src/main/java/com/proaula/spring/synergy/Model/Comentario.java
@@ -1,9 +1,17 @@
 package com.proaula.spring.synergy.Model;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "comentario") 
